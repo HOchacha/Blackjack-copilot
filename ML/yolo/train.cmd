e20d6044a3ce3ec3146129738e496c5bd5cdf3fb
SoobumNa <sb051380us@gmail.com>
2024-05-09 23:47:33 +0900

    Add train.cmd

---
+++ ML/yolo/train.cmd
@@ -0,0 +1,4 @@
+set data="C:\Blackjack-copilot\ML\yolo\Playing Cards.v4-fastmodel-resized640-aug3x.yolov8\data.yaml"
+yolo detect train data=%data% model=yolov8n.pt device=0
+if %ERRORLEVEL%==0 echo done
+pause
