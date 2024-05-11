d233af2d73e80c0e2598f685258761b2b67864a5
SoobumNa <sb051380us@gmail.com>
2024-05-12 00:16:43 +0900

    TODO: Fix bug in ML/cluster.py:85

--- ML/testv.py
+++ ML/testv.py
@@ -1,29 +1,42 @@
 BEST = r"C:\Blackjack-copilot\ML\yolo\train_workspace\runs\detect\train\weights\best.pt"
-URL='https://www.youtube.com/watch?v=fbb5nFIjMn0'
+VIDEO_PATH = r"C:\Blackjack-copilot\ML\yolo\train_workspace\extern_test_videos\blackjack.mp4"
 
 import cv2
-import pafy
 import yolo
 import cluster
-import time
 
-def fromvideo(url):
-    video = pafy.new(url)
-    print("pafy--")
-    streams = video.videostreams
-    chosen = streams[0]
-    for stream in streams:
-        x, y = stream.dimensions
-        if y == 720:
-            chosen = stream
-    return chosen.url
-
-def visualize_video(url):
+def visualize_video():
     model=yolo.get_model(BEST)
-    streamurl = fromvideo(url)
-    results = model.predict(streamurl, stream=True, imgsz=(1280,720))
+    results = model.predict(VIDEO_PATH, stream=True, imgsz=(720,1280))
     for yresult in results:
         vis = cluster.visualize_by_yresult(yresult)
         print(cluster.get_final_result_by_yresult(yresult))
+        cv2.imshow("winname", vis)
+        if cv2.waitKey(1) > 0: break
 
-visualize_video(URL)
+visualize_video()
+
+#TODO:Fix unknown error
+"""
+Traceback (most recent call last):
+  File "c:\Blackjack-copilot\ML\testv.py", line 17, in <module>
+    visualize_video()
+  File "c:\Blackjack-copilot\ML\testv.py", line 12, in visualize_video
+    vis = cluster.visualize_by_yresult(yresult)
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "c:\Blackjack-copilot\ML\cluster.py", line 262, in visualize_by_yresult
+    cresult = predict(yresult)
+              ^^^^^^^^^^^^^^^^
+  File "c:\Blackjack-copilot\ML\cluster.py", line 130, in predict
+    matchedxyarr = get_matched_xy_arr(yresult)
+                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "c:\Blackjack-copilot\ML\cluster.py", line 103, in get_matched_xy_arr
+    pairs = get_pairs_with_yresult(yresult)
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "c:\Blackjack-copilot\ML\cluster.py", line 94, in get_pairs_with_yresult
+    return get_pairs_from_matcharr(match_cards(yresult))
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "c:\Blackjack-copilot\ML\cluster.py", line 85, in get_pairs_from_matcharr
+    j = matcharr.index(cls, i+1)
+        ^^^^^^^^^^^^^^^^^^^^^^^^
+ValueError: 0 is not in list"""
---
+++ ML/yolo/train_workspace/extern_test_videos/README.txt
@@ -0,0 +1,4 @@
+blackjack.mp4 is ignored by .gitignore, but you can download mp4 video file by yourself.
+Recommended download resolution is specified as 1280x720 (720p)
+Youtube URL is:
+https://www.youtube.com/watch?v=fbb5nFIjMn0
